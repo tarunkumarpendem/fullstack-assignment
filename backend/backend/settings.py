91bd1adefde7b0ed55768b79f24bc7b201ec3622
tarunkumarpendem <ubuntu@ip-172-31-16-26.ec2.internal>
2024-11-01 13:18:58 +0000

    updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ FRONTEND_URL = os.environ["FRONTEND_URL"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '204.236.204.149']
+ALLOWED_HOSTS = ['localhost', '204.236.204.149', '54.82.7.42']
 
 # Application definition
 
